0f95a1f8406e0dfb65f9a48824a7fd29787330b2
Bill Cromie <bill@cromie.org>
2019-06-12 13:50:56 -0400

    turning off the persistent activation call update job.

--- app/jobs/activation_call_update_job.rb
+++ app/jobs/activation_call_update_job.rb
@@ -3,16 +3,17 @@
 class ActivationCallUpdateJob
   include Sidekiq::Worker
   # this needs work.
+  # should not be a persistent job, should be per call,
+  # and should be scheduled in the future to check and cleanup
+  # and to do so at intervals
 
+  # this exists because we need to make sure that the call is done
+  # and move on to the next check
   def perform(_useless)
     redis = Redis.current
     res = redis.get('ActivationCallUpdateLock')
     if res.nil?
       ActivationCall.ongoing.find_each do |activation_call|
-        
-        activation_call.call_status = activation_call.call.update.status
-        activation_call.save
-
         if activation_call.card.present?
           if !activation_call.card&.active? && activation_call.timeout_error?
             activation_call.failure
