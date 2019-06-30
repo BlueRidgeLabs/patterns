6a0dc7f84a325ce21df53b2dd84ba33aa5101808
data-doge <eugene@enspiral.com>
2019-06-30 16:54:46 -0400

    refactor specs

--- spec/services/giftrocket_service_spec.rb
+++ spec/services/giftrocket_service_spec.rb
@@ -6,17 +6,22 @@ RSpec.describe GiftrocketService do
   describe "#create_order!" do
     let(:person) { FactoryBot.create(:person) }
     let(:digital_gift) { FactoryBot.create(:digital_gift, amount_cents: 40_00, person: person) }
+    let(:gr_gift_id) { 'covfefe_gift_id' }
+    let(:gr_order_id) { 'covfefe_order_id' }
+    let(:gr_funding_source_id) { 'covfefe_fs_id' }
+    let(:gr_recipient_link) { 'https://covfefe.com' }
+    let(:gr_fee) { 1_00 }
     let(:mock_order) {
       Hashie::Mash.new({
-        id: 'covfefe_order_id',
+        id: gr_order_id,
         payment: {
-          fees: 1_00
+          fees: gr_fee
         },
         gifts: [{
-          id: 'covfefe_gift_id',
+          id: gr_gift_id,
           raw: {
             recipient: {
-              link: 'https://covfefe.com'
+              link: gr_recipient_link
             }
           }
         }]
@@ -26,7 +31,7 @@ RSpec.describe GiftrocketService do
     before do
       mock_balance_funding_source = double("mock_balance_funding_source")
       allow(mock_balance_funding_source).to receive(:method).and_return('balance')
-      allow(mock_balance_funding_source).to receive(:id).and_return('covfefe_fs_id')
+      allow(mock_balance_funding_source).to receive(:id).and_return(gr_funding_source_id)
       mock_funding_sources = [mock_balance_funding_source]
       allow(Giftrocket::FundingSource).to receive(:list).and_return(mock_funding_sources)
     end
@@ -39,7 +44,7 @@ RSpec.describe GiftrocketService do
       }.to_json
       expect(Giftrocket::Order).to receive(:create!).with({
         external_id: expected_external_id,
-        funding_source_id: 'covfefe_fs_id',
+        funding_source_id: gr_funding_source_id,
         campaign_id: ENV['GIFTROCKET_HIGH_CAMPAIGN'],
         gifts: [
           amount: digital_gift.amount.to_s,
@@ -52,12 +57,12 @@ RSpec.describe GiftrocketService do
       result = sut.create_order!(digital_gift)
       expect(result).to eq({
         external_id: expected_external_id,
-        funding_source_id: 'covfefe_fs_id',
+        funding_source_id: gr_funding_source_id,
         campaign_id: ENV['GIFTROCKET_HIGH_CAMPAIGN'],
-        fee: 1_00,
-        order_id: 'covfefe_order_id',
-        gift_id: 'covfefe_gift_id',
-        link: 'https://covfefe.com',
+        fee: gr_fee,
+        order_id: gr_order_id,
+        gift_id: gr_gift_id,
+        link: gr_recipient_link,
         order_details: Base64.encode64(Marshal.dump(mock_order))
       })
     end
