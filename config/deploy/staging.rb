b6c7a0f050f3de3d339a1c1438e22740d1439994
Bill Cromie <bill@cromie.org>
2020-02-09 15:47:32 -0500

    moving to dotenv and more twelvefactor like

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,5 +10,5 @@ server ENV['STAGING_SERVER'], :app, :web, :db, primary: true
 
 task :link_env_var do
   # pull in database.yml on server
-  run "rm -f #{release_path}/config/local_env.yml && ln -s #{deploy_to}/shared/local_env.yml #{release_path}/config/local_env.yml"
+  #run "rm -f #{release_path}/config/local_env.yml && ln -s #{deploy_to}/shared/local_env.yml #{release_path}/config/local_env.yml"
 end
