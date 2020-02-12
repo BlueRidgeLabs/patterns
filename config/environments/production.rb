6b98670ce911b4116c79907fabc99cab8ecbef8b
Bill Cromie <bill@cromie.org>
2020-02-12 10:22:46 -0500

    only remote ip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Patterns::Application.configure do
   config.log_level = :info
   config.lograge.enabled = true
   config.lograge.custom_options = lambda do |event|
-    {remote_ip: event.payload[:remote_ip], user_id: event.payload[:user_id], params: event.payload[:params].except('controller', 'action', 'format', 'utf8')}
+    {remote_ip: event.payload[:remote_ip]}
   end
   # Analytics
   config.google_analytics_enabled = true
