2c78f6fd33b9fbf1777744cba58d41e8c33326e9
Bill Cromie <bill@cromie.org>
2020-03-16 09:26:44 -0400

    adding active storage

---
+++ app/mailboxes/application_mailbox.rb
@@ -0,0 +1,4 @@
+class ApplicationMailbox < ActionMailbox::Base
+  # this is security through obscurity
+  routing /^signed\.consent\.#{ENV['MANDRILL_WEBHOOK_SECRET_KEY']}@/i     => :consent_form
+end
