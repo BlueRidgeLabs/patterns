3aab44e409170dbdef4e5c688fb562036c734486
Bill Cromie <bill@cromie.org>
2020-07-13 18:12:44 -0400

    wat

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
-if ENV['REDIS_URL'].blank?
-  Redis.new(host: 'localhost', port: 6379)
-else
-  Redis.new(url: ENV['REDIS_URL'])
-end
+Redis.current =  if ENV['REDIS_URL'].blank?
+    Redis.new(host: 'localhost', port: 6379)
+  else
+    Redis.new(url: ENV['REDIS_URL'])
+ end
 
 # LockManager = Redlock::Client.new([ "redis://127.0.0.1:6379"])
 
