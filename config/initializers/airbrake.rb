ab6a4ce3b1848be16a0936dde4deb8a4165e0d22
Bill Cromie <bill@cromie.org>
2021-01-20 14:35:51 -0500

    oops, wrong key for airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -15,7 +15,7 @@ Airbrake.configure do |c|
   # from the right sidebar.
   # https://github.com/airbrake/airbrake-ruby#project_id--project_key
   c.project_id = Rails.application.credentials.airbrake[:project_id] 
-  c.project_key = Rails.application.credentials.airbrake[:project_key]
+  c.project_key = Rails.application.credentials.airbrake[:key]
 
   # Configures the root directory of your project. Expects a String or a
   # Pathname, which represents the path to your project. Providing this option
