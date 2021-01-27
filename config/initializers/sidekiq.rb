b94db1f1dce52f649dc5407f88b679a61933c72b
Bill Cromie <bill@cromie.org>
2021-01-27 18:56:41 -0500

    ok, another try

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -11,5 +11,9 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = Redis.current
+  config.redis = if ENV['REDIS_URL'].blank?
+                   { host: 'localhost', port: 6379, timeout: 2 }
+                 else
+                   { url: ENV['REDIS_URL'], timeout: 2 }
+                 end
 end
