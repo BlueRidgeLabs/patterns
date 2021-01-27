fb17a89252c30700c35a3e34b99225d133558ac6
Bill Cromie <bill@cromie.org>
2021-01-27 18:28:56 -0500

    ok, another try

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = Redis.current
+  config.redis = if ENV['REDIS_URL'].blank?
+                   { host: 'localhost', port: 6379, timeout: 2 }
+                 else
+                   { url: ENV['REDIS_URL'], timeout: 2 }
+                 end
+
   config.average_scheduled_poll_interval = 1 # poll every second
 end
 
