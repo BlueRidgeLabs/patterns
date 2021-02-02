f188827bf853e4f7a0de4c822380bf1b00e7e429
Bill Cromie <bill@cromie.org>
2021-02-02 13:52:44 -0500

    how to use mock_redis. may make test setup easier/faster

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,4 +4,7 @@ Redis.current ||= if ENV['REDIS_URL'].blank?
                     Redis.new(host: 'localhost', port: 6379, timeout: 2)
                   else
                     Redis.new(url: ENV['REDIS_URL'], timeout: 2)
-                end
+                  end
+
+# may make testing setups simpler, tbd.
+# Redis.current = MockRedis.new if Rails.env.test? && defined?(MockRedis)
