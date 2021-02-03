f4e9f21bae41f2cd4b0bbadc5c2a901c274ceeb4
Bill Cromie <bill@cromie.org>
2021-02-03 15:50:57 -0500

    adding testing public and private keys, for testing

--- spec/services/s3_backup_service_spec.rb
+++ spec/services/s3_backup_service_spec.rb
@@ -3,12 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe S3BackupService do
-  let(:sut) { described_class }
+  let(:sut) { described_class.new }
 
   xdescribe 'sends expected args to s3' do
     xit 'works' do
-      expect(Rails.logger.info).to receive('Object uploaded')
-      sut.upload('spec/fixtures/encryption_test.txt')
+      # sut.upload(file_fixture('encryption_test.txt').to_s)
+      # sut.download('encryption_text.txt',file_fixture('encryption_test.txt').to_s,"#{Rails.root.to_s}/tmp/")
+      # check are they the same?
+      # probably using vcr, I think.
     end
 
     xit 'does not work' do
