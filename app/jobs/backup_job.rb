b1e12abaf1dc0b1922537f4a948d526c00ec8a09
Bill Cromie <bill@cromie.org>
2021-02-03 19:28:45 -0500

    ok, maybe this will work

--- app/jobs/backup_job.rb
+++ app/jobs/backup_job.rb
@@ -9,8 +9,8 @@ class BackupJob
     s3 = S3BackupService.new
     filename = "patterns-#{Rails.env}-#{Time.zone.now.strftime('%Y%m%dT%H%M')}.sql"
     path = '/tmp/'
-    filepath = path + filename
-    system("mysqldump -u #{config['username']} -h #{config['host']} -p#{config['password']} #{config['database']} > gzip -9 #{path}#{filename}.gz")
-    s3.upload("#{filepath}.gz")
+    filepath = "#{path}#{filename}.gz"
+    system("mysqldump -u #{config['username']} -h #{config['host']} -p#{config['password']} #{config['database']} | gzip > #{filepath}")
+    s3.upload(filepath)
   end
 end
